6e32d1d2f653ac458faaf2e09c2d1c26525afd2b
Hoang NT <hoangnt@acm.org>
2016-01-27 11:56:12 +0900

    Fio XBee sub-project

---
+++ Hardware/fio_blink/fio_blink.ino
@@ -0,0 +1,34 @@
+int incomingByte = 0;
+
+void setup() {
+  Serial.begin(57600);
+  pinMode(13, OUTPUT);
+
+  // blink twice at start up
+  digitalWrite(13, LOW);
+  delay(1000);
+
+  digitalWrite(13, HIGH);
+  delay(50);
+  digitalWrite(13, LOW);
+  delay(200);
+  digitalWrite(13, HIGH);
+  delay(50);
+  digitalWrite(13, LOW);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+
+   if (incomingByte == '0') {
+      digitalWrite(13, LOW);
+    } else if (incomingByte == '1') {
+     digitalWrite(13, HIGH);
+    }
+
+  Serial.print("Fio received: ");
+  Serial.write(incomingByte);
+  Serial.write(10);
+  }
+}
